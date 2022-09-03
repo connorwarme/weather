d04b409ee1a1e94f21459dd2674c60ee84b6dde7
connorwarme <connor.warme@gmail.com>
2022-09-03 10:26:30 -0600

    Write fetch for current weather and airQ, need to test

--- src/index.js
+++ src/index.js
@@ -35,35 +35,61 @@ submitBtn.addEventListener("click", () => {
 // easier to keep them seperate?
 const apiAction = (() => {
   const currentValue = "917a17f77a60ae96ee081212e94e3f75";
-  const getWeatherData = async (location) => {
+  const getForecast = async (location) => {
     const searchLocation = location;
-    const megaData = await fetch(
+    const forecastFetch = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${searchLocation}&appid=${currentValue}&units=imperial`,
       {
         mode: "cors",
       }
     );
-    const forecastData = await megaData.json();
+    const forecastData = await forecastFetch.json();
     return forecastData;
   };
+  const getCurrent = async (location) => {
+    // !!! need to check api call - accurate for current conditions?
+    const currentFetch = await fetch(
+      `https://api.openweathermap.org/data/2.5/current?q=${location}&appid=${currentValue}&units=imperial`,
+      {
+        mode: "cors",
+      }
+    );
+    const currentData = await currentFetch.json();
+    return currentData;
+  };
+  const getAirQ = async (location) => {
+    const airFetch = await fetch( `https://api.openweathermap.org/data/2.5/current?q=${location}&appid=${currentValue}&units=imperial`,
+    {
+      mode: "cors",
+    }
+    );
+    const airData = await airFetch.json();
+    return airData;
+  }
+  }
   // get all the datas - thinking an array with 3 obj: [{current}, {forecast}, {airq}]
   // 
-  const declareData = (input) => {
-    const city = input.city.name;
-    const country = input.city.country;
-    const temp = input.list[0].main.temp;
-    const conditions = input.list[0].weather[0].main;
-    const description = input.list[0].weather[0].description;
-    const wind = input.list[0].wind;
-    const pop = input.list[0].pop;
-    const sunrise = input.city.sunrise;
-    const sunset = input.city.sunset;
-    const humidity = input.list[0].main.humidity;
-    const feelsLike = input.list[0].main.feels_like;
-    const visibility = input.list[0].visibility;
-    const precip3h = input.list[0].rain;
+  const declareData = (current, forecast, air) => {
+    // !!! need to confirm current data, find airq, and think thru what else should be included
+    // city, country, current temp, high, low, current conditions & descrrption, current weather icon?,
+    // 
+    const city = current.city.name;
+    const country = current.city.country;
+    const temp = current.list[0].main.temp;
+    const high = "";
+    const low = "";
+    const conditions = forecast.list[0].weather[0].main;
+    const description = forecast.list[0].weather[0].description;
+    const wind = forecast.list[0].wind;
+    const pop = forecast.list[0].pop;
+    const sunrise = forecast.city.sunrise;
+    const sunset = forecast.city.sunset;
+    const humidity = forecast.list[0].main.humidity;
+    const feelsLike = forecast.list[0].main.feels_like;
+    const visibility = forecast.list[0].visibility;
+    const precip3h = forecast.list[0].rain;
     const airQuality = "need other api";
-    const pressure = input.list[0].main.pressure;
+    const pressure = forecast.list[0].main.pressure;
     return {city, country, temp, conditions, description, wind, pop, sunrise, sunset, humidity, feelsLike, visibility, precip3h, airQuality, pressure};
   };
 
